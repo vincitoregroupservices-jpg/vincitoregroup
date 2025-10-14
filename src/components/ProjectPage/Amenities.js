742d29b913768bc44b54785a9b834cbf466dbbf6
siddharaj <gohilsiddharaj13325@gmail.com>
2025-10-14 22:08:44 +0530

    Aminity added

--- src/components/ProjectPage/Amenities.js
+++ src/components/ProjectPage/Amenities.js
@@ -21,21 +21,18 @@ const Amenities = ({ project }) => {
     <div>
       <SectionTitle text={`World-Class Amenities at ${project.name}`} />
 
-      <div className="relative">
+      <div className="relative mt-10 px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32">
         <div ref={scrollRef} className={styles.amenitiesCarousel}>
           {project.amenities.map((item, index) => (
             <div key={index} className={styles.amenityCard}>
-              <div className="h-14 w-14 md:h-20 md:w-20 rounded-full flex items-center justify-center bg-gold p-3 md:p-5">
-                {/* <Image
-                  src={`/amenities/gym.svg`}
-                  width={40}
-                  height={40}
-                  className="h-full w-auto"
-                  alt={item.name}
-                /> */}
-                <span className="material-symbols-outlined h-full w-auto bg-red-200 text-[#111]">fitness_center</span>
+              <div className="h-14 w-14 md:h-20 md:w-20 rounded-full flex items-center justify-center bg-gold">
+                <span className="material-symbols-outlined text-3xl w-auto text-[#111]">
+                  {item.icon}
+                </span>
               </div>
-              <p className="mt-2 text-xs md:text-lg font-medium">{item.name}</p>
+              <p className="mt-2 text-xs md:text-lg font-medium">
+                {item.label}
+              </p>
             </div>
           ))}
         </div>
