60ffaf8c34a9be146d05294eb73c491003df1024
siddharaj <gohilsiddharaj13325@gmail.com>
2025-10-14 15:52:39 +0530

    Updated

--- src/components/ProjectPage/CTASection.js
+++ src/components/ProjectPage/CTASection.js
@@ -8,29 +8,28 @@ const CTASection = ({ project }) => {
     <div
       className="relative flex flex-col items-center justify-center text-center py-20"
       style={{
-        backgroundImage: `url(/${project.thumbnail})`,
+        backgroundImage: `url(${project.thumbnail})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
       {/* Dark overlay */}
       <div className="absolute inset-0 bg-black opacity-80"></div>
-      <Section>
-        {/* Content */}
-        <div className="relative flex flex-col items-center gap-6 text-white">
-          <h1 className="text-3xl md:text-5xl font-semibold max-w-3xl">
-            {project.tagline.quate}
-          </h1>
-          <div className="flex flex-wrap justify-center gap-3">
-            <Link href={`/contact`}>
-              <Button text="Get in Touch" outline noArrow />
-            </Link>
-            <Link href={`tel:${project.contact[0]}`}>
-              <Button text="Enquire Now" />
-            </Link>
-          </div>
+
+      {/* Content */}
+      <div className="relative flex flex-col items-center gap-6 text-white">
+        <h1 className="text-3xl md:text-5xl font-semibold max-w-3xl">
+          {project.tagline.quate}
+        </h1>
+        <div className="flex flex-wrap justify-center gap-3">
+          <Link href={`/contact`}>
+            <Button text="Get in Touch" outline noArrow />
+          </Link>
+          <Link href={`tel:${project.contact[0]}`}>
+            <Button text="Enquire Now" />
+          </Link>
         </div>
-      </Section>
+      </div>
     </div>
   );
 };
